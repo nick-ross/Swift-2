07f3adb1d77859882822980335a357751ff79dfe
nick-ross <mx5433@gmail.com>
2014-08-20 09:36:20 -0500

    Initial Commit

---
+++ Swift 2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift 2
+//
+//  Created by Nick Ross on 8/20/14.
+//  Copyright (c) 2014 Nick Ross. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
